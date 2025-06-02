41a9287252b23b43e7c102a7e0b1dc00d4456aa9
reda0206 <cjones7@ltu.edu>
2025-06-02 19:13:50 -0400

    Added Pause, Main, and Game Over Menu

---
+++ Assets/Scripts/ButtonsGameOver.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonsGameOver : MonoBehaviour
+{
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+        
+    public void QuitToMenuButton()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public bool isPaused = false;
+    public GameObject pauseMenuUi;
+    private List<AudioSource> audioSources = new List<AudioSource>();
+    public List<AudioSource> excludedAudioSources = new List<AudioSource>();
 
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -27,6 +31,67 @@ public class GameManager : MonoBehaviour
     
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+    public void Resume()
+    {
+        isPaused = false;
+        pauseMenuUi.SetActive(false);
+        ResumeAudio();
+        Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        pauseMenuUi.SetActive(true);
+        PauseAudio();
+        Time.timeScale = 0f;
+    }
+    public void PauseAudio()
+    {
+        // AudioListener.pause = true;
+
+        foreach (AudioSource audio in FindObjectsOfType<AudioSource>())
+        {
+            if (audio.isPlaying && !excludedAudioSources.Contains(audio))
+            {
+                audio.Pause();
+                audioSources.Add(audio);
+            }
+        }
+    }
+
+    public void ResumeAudio()
+    {
+        // AudioListener.pause = false;
+        for (int i = audioSources.Count - 1; i >= 0; i--)
+        {
+            if (audioSources[i])
+            {
+                audioSources[i].UnPause();
+                audioSources.RemoveAt(i);
+            }
+        }
+    }
+    public void BackToMenuButton()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+        Time.timeScale = 1f;
+    }
+
+    public void ExitGameButton()
+    {
+        Application.Quit();
     }
 }
