01537aa7935eb2968c6afaf7916d38db022b4e10
reda0206 <cjones7@ltu.edu>
2025-06-03 14:11:02 -0400

    Made apples fall at a static speed

--- Assets/Scripts/FallingApples.cs
+++ Assets/Scripts/FallingApples.cs
@@ -5,13 +5,35 @@ using UnityEngine;
 public class FallingApples : MonoBehaviour
 {
     public float fallSpeed = 2f;
-    private void OnTriggerStay2D(Collider2D collision)
+    public float minX = -8f;
+    public float maxX = 8f;
+    public float minY = 4f;
+    public float maxY = 6f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             GameManager.instance.CollectApples();
-            Destroy(gameObject);
-            fallSpeed = 2f + 0.1f;
+            TeleportToRandomPosition();
+            fallSpeed = fallSpeed + 0.1f;
         }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+           collision.gameObject.GetComponent<PlayerMovement>().TakeDamage(1);
+            TeleportToRandomPosition();
+        }
+    }
+
+    private void TeleportToRandomPosition()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        transform.position = new Vector2(randomX, randomY);
     }
 }
